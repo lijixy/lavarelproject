11fbf93697bca630392a0da2d5ae978880e3e59e
Mark <Mark@Mark-PC>
2019-03-27 08:24:04 +0800

    修改

---
+++ app/Http/Controllers/Api/testController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class testController extends Controller{
+	public function testFirst(){
+		var_dump("fdasf");
+	}
+}
+
+
+?>
